589512fdccb9559cd385da9b69cc417c0e10a204
Amit Kumar <amitkv1212@gmail.github.com>
2021-02-19 22:22:46 +0530

    Added files till video 2

---
+++ app/src/main/java/com/aips/amit/amite_commerceapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.aips.amit.amite_commerceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/aips/amit/amite_commerceapp/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.aips.amit.amite_commerceapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
+    }
+}
