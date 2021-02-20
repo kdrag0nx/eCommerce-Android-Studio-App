95bbe213a24ad9138253a6b45758b962a5efadaa
Amit Kumar <amitkv1212@gmail.github.com>
2021-02-20 18:27:56 +0530

    Clip 2 Update

--- app/src/main/java/com/aips/amit/amite_commerceapp/MainActivity.java
+++ app/src/main/java/com/aips/amit/amite_commerceapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.aips.amit.amite_commerceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frameLayout = findViewById(R.id.register_frame_layout);
+        setFragemnt(new SigninFragment());
+    }
+
+    private void setFragemnt(Fragment fragemnt) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(frameLayout.getId(), fragemnt);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/aips/amit/amite_commerceapp/SigninFragment.java
+++ app/src/main/java/com/aips/amit/amite_commerceapp/SigninFragment.java
@@ -1,62 +1,54 @@
 package com.aips.amit.amite_commerceapp;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SigninFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SigninFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public SigninFragment() {
         // Required empty public constructor
     }
+    private TextView donthaveanaccount;
+    private FrameLayout parentframeLayout;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SigninFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SigninFragment newInstance(String param1, String param2) {
-        SigninFragment fragment = new SigninFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signin, container, false);
+        View view =  inflater.inflate(R.layout.fragment_signin, container, false);
+        donthaveanaccount = view.findViewById(R.id.tv_dont_have_an_account);
+        parentframeLayout = getActivity().findViewById(R.id.register_frame_layout);
+        return view;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        donthaveanaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFragement(new SignUpFragment());
+            }
+        });
+    }
+
+    private void setFragement(Fragment fragement) {
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(parentframeLayout.getId(),fragement);
+        fragmentTransaction.setCustomAnimations(R.anim.slide_from_right, R.anim.slideout_from_left);
+        fragmentTransaction.commit();
     }
 }
